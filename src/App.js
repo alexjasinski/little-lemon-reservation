25f83477a6a5697a60f994489a6708206f577ef8
Alexander Jasinski <design.jasinski@gmail.com>
2024-04-07 19:14:05 +0200

    implemented basic structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import logo from './assets/logo.png';
 import './App.css';
+// import './components/About';
+// import './components/Footer';
+// import './components/Hero';
+// import './components/Highlights';
+// import './components/Nav';
+// import './components/Testimonials'
 
 function App() {
   return (
-    <div className="App">
-      Some text
-    </div>
+    <>
+      <Nav>
+        <nav>
+          <img>{logo}</img>
+          <ul>
+            <li href="/home">Home</li>
+            <li href="/about">About</li>
+            <li href="/menu">Menu</li>
+            <li href="/reservation">Reservation</li>
+            <li href="/order">Order online</li>
+          </ul>
+        </nav>
+      </Nav>
+      <main>
+        <Hero />
+        <Highlights />
+        <Testimonials />
+        <About />
+      </main>
+      <footer></footer>
+    </>
   );
 }
 
